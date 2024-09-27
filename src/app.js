3de409f95662f53fa0397fe5f8121e62b260b7c0
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2024-09-27 13:28:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let doc = document.getElementById("excuse");
+
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  let text =
+    get_random(who) + get_random(action) + get_random(what) + get_random(when);
+  doc.innerHTML = text;
 };
+
+function get_random(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
